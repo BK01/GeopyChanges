a028fbe22b3f38c3da728a71dcfdf71a12bdf715
ilona-asa <asailona@gmail.com>
2016-12-15 12:02:03 +0100

    repair DataBC return non-existing address

--- geopy/geocoders/databc.py
+++ geopy/geocoders/databc.py
@@ -109,9 +109,8 @@ class DataBC(Geocoder):
     def _parse_feature(feature):
         properties = feature['properties']
         coordinates = feature['geometry']['coordinates']
-        #print properties['fullAddress']
         if properties['fullAddress'] == 'BC':
-            properties['fullAddress'] = 'None'
+            return None
         return Location(
             properties['fullAddress'], (coordinates[1], coordinates[0]),
             properties
