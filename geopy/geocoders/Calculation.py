ae50ff514b87e1addbab257033246256985763de
michaelws92 <michael.wsaputra@gmail.com>
2016-12-15 13:53:43 +0100

    Revision 2

--- geopy/geocoders/Calculation.py
+++ geopy/geocoders/Calculation.py
@@ -1,17 +1,19 @@
 from geopy.distance import vincenty
 def calculations(url,places):
-	tempplaces= ""
 	tempdistance = []
+	temparray = []
+	#print places
 	for gl in range(len(places)):
-		distance = vincenty(url,(places[gl].latitude,places[gl].longitude)).km
-		tempdistance.append(distance)
+		#print gl
+		tempdistance.append((vincenty(url,(places[gl].latitude,places[gl].longitude)),places[gl]))
+	tempdistance.sort()
+	
+	for i in range(len(tempdistance)):
+		#print tempdistance[i]
+		tempdistance[i] =  tempdistance[i][1]
 
-	for i in range(len(tempdistance)-1):
-		for j in range(len(tempdistance)-1):
-			if tempdistance[j] > tempdistance[j+1]:
-				tempplaces = places[j]
-				places[j] = places[j+1]
-				places[j+1]= tempplaces
 
-	return places
+	#print tempdistance
+	#print tempdistance
+	return tempdistance
 				
